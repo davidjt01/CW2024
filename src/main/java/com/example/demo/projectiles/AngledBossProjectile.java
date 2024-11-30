b54ed5b5e7636416c41120a0e75152b18ff9338b
davidjt <davidjt001@yahoo.com>
2024-12-01 05:58:32 +0800

    added AngledBossProjectile

---
+++ src/main/java/com/example/demo/projectiles/AngledBossProjectile.java
@@ -0,0 +1,19 @@
+package com.example.demo.projectiles;
+
+public class AngledBossProjectile extends BossProjectile {
+
+    private final double angle;
+
+    public AngledBossProjectile(double initialYPos, double angle) {
+        super(initialYPos);
+        this.angle = Math.toRadians(angle);
+    }
+
+    @Override
+    public void updatePosition() {
+        double dx = Math.cos(angle) * HORIZONTAL_VELOCITY;
+        double dy = Math.sin(angle) * HORIZONTAL_VELOCITY;
+        super.moveHorizontally(dx);
+        super.moveVertically(dy);
+    }
+}
