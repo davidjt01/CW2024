33137e97ad2a6c28700317489b9e9591936ed803
davidjt <davidjt001@yahoo.com>
2024-12-04 06:40:10 +0800

    updated LevelFive to also spawn EnemyPlane and AdvancedEnemyPlane together with FinalBossPlane

--- src/main/java/com/example/demo/levels/LevelFive.java
+++ src/main/java/com/example/demo/levels/LevelFive.java
@@ -1,5 +1,8 @@
 package com.example.demo.levels;
 
+import com.example.demo.entities.DestructibleEntity;
+import com.example.demo.planes.EnemyPlane;
+import com.example.demo.planes.AdvancedEnemyPlane;
 import com.example.demo.planes.FinalBossPlane;
 import com.example.demo.levelui.LevelUI;
 import com.example.demo.levelui.FinalBossLevelUI;
@@ -9,8 +12,11 @@ public class LevelFive extends LevelParent {
     private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background2.jpg";
     private static final String NEXT_LEVEL = "com.example.demo.levels.LevelThree";
     private static final int PLAYER_INITIAL_HEALTH = 5;
+    private static final double ENEMY_SPAWN_PROBABILITY = .20;
+    private static final int TOTAL_ENEMIES = 5;
     private final FinalBossPlane finalBossPlane;
     private FinalBossLevelUI levelView;
+    private boolean spawnEnemyPlaneNext = true;
 
     public LevelFive(double screenHeight, double screenWidth) {
         super(BACKGROUND_IMAGE_NAME, screenHeight, screenWidth, PLAYER_INITIAL_HEALTH);
@@ -35,6 +41,24 @@ public class LevelFive extends LevelParent {
     protected void spawnEnemyUnits() {
         if (getCurrentNumberOfEnemies() == 0) {
             addEnemyUnit(finalBossPlane);
+        }
+
+        int currentNumberOfEnemies = getCurrentNumberOfEnemies();
+        for (int i = 0; i < TOTAL_ENEMIES - currentNumberOfEnemies; i++) {
+            if (Math.random() < ENEMY_SPAWN_PROBABILITY) {
+                double newEnemyInitialYPosition = Math.random() * getEnemyMaximumYPosition();
+
+                DestructibleEntity newEnemy;
+                if (spawnEnemyPlaneNext) {
+                    newEnemy = new EnemyPlane(getScreenWidth(), newEnemyInitialYPosition);
+                } else {
+                    newEnemy = new AdvancedEnemyPlane(getScreenWidth(), newEnemyInitialYPosition);
+                }
+
+                spawnEnemyPlaneNext = !spawnEnemyPlaneNext;
+
+                addEnemyUnit(newEnemy);
+            }
         }
     }
 
