dcb8cd06215dc787cfc6f9897e3be8786d4edf83
davidjt <davidjt001@yahoo.com>
2024-12-04 06:04:58 +0800

    updated RandomMovingBossProjectile for projectile to only change vertical velocity after a given time

--- src/main/java/com/example/demo/projectiles/RandomMovingBossProjectile.java
+++ src/main/java/com/example/demo/projectiles/RandomMovingBossProjectile.java
@@ -4,21 +4,32 @@ import java.util.Random;
 
 public class RandomMovingBossProjectile extends BossProjectile {
     private static final int VERTICAL_VELOCITY = 5;
+    private static final int FRAMES_BEFORE_DIRECTION_CHANGE = 30;
     private final Random random;
+    private int currentDirection;
+    private int framesInCurrentDirection;
 
     public RandomMovingBossProjectile(double initialYPos) {
         super(initialYPos);
         this.random = new Random();
+        this.currentDirection = getRandomDirection();
+        this.framesInCurrentDirection = 0;
     }
 
     @Override
     public void updatePosition() {
         moveHorizontally(HORIZONTAL_VELOCITY);
-        moveVertically(getRandomVerticalMove());
+        moveVertically(currentDirection * VERTICAL_VELOCITY);
+        framesInCurrentDirection++;
+
+        if (framesInCurrentDirection >= FRAMES_BEFORE_DIRECTION_CHANGE) {
+            currentDirection = getRandomDirection();
+            framesInCurrentDirection = 0;
+        }
     }
 
-    private double getRandomVerticalMove() {
-        return (random.nextBoolean() ? 1 : -1) * VERTICAL_VELOCITY;
+    private int getRandomDirection() {
+        return random.nextBoolean() ? 1 : -1;
     }
 
     @Override
