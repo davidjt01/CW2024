cbc7e8386b01ccf02fd466f174c1582462d0293a
davidjt <davidjt001@yahoo.com>
2024-12-04 06:02:29 +0800

    updated LevelFour to spawn SecondBossPlane

--- src/main/java/com/example/demo/levels/LevelFour.java
+++ src/main/java/com/example/demo/levels/LevelFour.java
@@ -1,6 +1,6 @@
 package com.example.demo.levels;
 
-import com.example.demo.planes.BossPlane;
+import com.example.demo.planes.SecondBossPlane;
 import com.example.demo.levelui.LevelUI;
 import com.example.demo.levelui.BossLevelUI;
 
@@ -9,14 +9,14 @@ public class LevelFour extends LevelParent {
     private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background2.jpg";
     private static final String NEXT_LEVEL = "com.example.demo.levels.LevelFive";
     private static final int PLAYER_INITIAL_HEALTH = 5;
-    private final BossPlane bossPlane1;
-    private final BossPlane bossPlane2;
+    private final SecondBossPlane bossPlane1;
+    private final SecondBossPlane bossPlane2;
     private BossLevelUI levelView;
 
     public LevelFour(double screenHeight, double screenWidth) {
         super(BACKGROUND_IMAGE_NAME, screenHeight, screenWidth, PLAYER_INITIAL_HEALTH);
-        bossPlane1 = new BossPlane(levelView);
-        bossPlane2 = new BossPlane(levelView);
+        bossPlane1 = new SecondBossPlane(levelView);
+        bossPlane2 = new SecondBossPlane(levelView);
     }
 
     @Override
