1375c54a05ff0f0808b5efd593afa32482f32109
davidjt <davidjt001@yahoo.com>
2024-12-07 18:39:23 +0800

    revert commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,18 +1,8 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.media;
-    requires transitive javafx.graphics;
 
+
+    opens com.example.demo to javafx.fxml;
     exports com.example.demo.controller;
-    opens com.example.demo.levels to javafx.fxml;
-    opens com.example.demo.images to javafx.fxml;
-    opens com.example.demo.levelui to javafx.fxml;
-    opens com.example.demo.controller to javafx.fxml;
-    opens com.example.demo.entities to javafx.fxml;
-    opens com.example.demo.audio to javafx.fxml;
-    opens com.example.demo.projectiles to javafx.fxml;
-    opens com.example.demo.planes to javafx.fxml;
-    exports com.example.demo.menus;
-    opens com.example.demo.menus to javafx.fxml;
 }
