6496a0fb9d6df57f3f37f790f32e2c3b0c64560c
davidjt <davidjt001@yahoo.com>
2024-12-08 08:04:25 +0800

    adjusted IMAGE HEIGHT in EnemyPlane

--- src/main/java/com/example/demo/planes/EnemyPlane.java
+++ src/main/java/com/example/demo/planes/EnemyPlane.java
@@ -6,7 +6,7 @@ import com.example.demo.projectiles.EnemyProjectile;
 public class EnemyPlane extends Plane {
 
 	private static final String IMAGE_NAME = "enemyplane.png";
-	private static final int IMAGE_HEIGHT = 150;
+	private static final int IMAGE_HEIGHT = 54;
 	private static final int HORIZONTAL_VELOCITY = -6;
 	private static final double PROJECTILE_X_POSITION_OFFSET = -100.0;
 	private static final double PROJECTILE_Y_POSITION_OFFSET = 50.0;
