29e8bfe3ef869f6ac67c95a464239e2844a5503b
davidjt <davidjt001@yahoo.com>
2024-12-10 00:47:07 +0800

    added method showGameOver method in GameOverDisplay and added attributes HEIGHT and WIDTH to adjust Game Over image size

--- src/main/java/com/example/demo/displays/GameOverDisplay.java
+++ src/main/java/com/example/demo/displays/GameOverDisplay.java
@@ -7,11 +7,20 @@ public class GameOverDisplay extends ImageView {
 	
 	private static final String IMAGE_NAME = "/com/example/demo/images/gameover.png";
 
+	private static final int HEIGHT = 500;
+	private static final int WIDTH = 600;
+
 	public GameOverDisplay(double xPosition, double yPosition) {
-		setImage(new Image(getClass().getResource(IMAGE_NAME).toExternalForm()) );
-//		setImage(ImageSetUp.getImageList().get(ImageSetUp.getGameOver()));
-		setLayoutX(xPosition);
-		setLayoutY(yPosition);
+		this.setImage(new Image(getClass().getResource(IMAGE_NAME).toExternalForm()));
+		this.setVisible(false);
+		this.setFitHeight(HEIGHT);
+		this.setFitWidth(WIDTH);
+		this.setLayoutX(xPosition);
+		this.setLayoutY(yPosition);
+	}
+
+	public void showGameOver() {
+		this.setVisible(true);
 	}
 
 }
