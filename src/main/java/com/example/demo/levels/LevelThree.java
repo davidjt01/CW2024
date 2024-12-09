2da2cb6de2a5c99b2cb8c15232363fde808e9d6d
davidjt <davidjt001@yahoo.com>
2024-12-09 20:01:29 +0800

    updated LevelThree to alternate between spawning EnemyPlane and AdvancedEnemyPlane

--- src/main/java/com/example/demo/levels/LevelThree.java
+++ src/main/java/com/example/demo/levels/LevelThree.java
@@ -2,16 +2,18 @@ package com.example.demo.levels;
 
 import com.example.demo.entities.DestructibleEntity;
 import com.example.demo.levelviews.LevelView;
+import com.example.demo.planes.AdvancedEnemyPlane;
 import com.example.demo.planes.EnemyPlane;
 
 public class LevelThree extends LevelParent {
 
     private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background1.jpg";
-    private static final String NEXT_LEVEL = "com.example.demo.levels.LevelFour";
+    private static final String NEXT_LEVEL = "com.example.demo.levels.LevelTwo";
     private static final int TOTAL_ENEMIES = 5;
     private static final int KILLS_TO_ADVANCE = 10;
     private static final double ENEMY_SPAWN_PROBABILITY = .20;
     private static final int PLAYER_INITIAL_HEALTH = 5;
+    private int enemyCounter = 0; // Counter to alternate between enemy types
 
     public LevelThree(double screenHeight, double screenWidth) {
         super(BACKGROUND_IMAGE_NAME, screenHeight, screenWidth, PLAYER_INITIAL_HEALTH);
@@ -21,9 +23,9 @@ public class LevelThree extends LevelParent {
     protected void checkIfGameOver() {
         if (userIsDestroyed()) {
             loseGame();
-        }
-        else if (userHasReachedKillTarget())
+        } else if (userHasReachedKillTarget()) {
             goToNextLevel(NEXT_LEVEL);
+        }
     }
 
     @Override
@@ -37,8 +39,14 @@ public class LevelThree extends LevelParent {
         for (int i = 0; i < TOTAL_ENEMIES - currentNumberOfEnemies; i++) {
             if (Math.random() < ENEMY_SPAWN_PROBABILITY) {
                 double newEnemyInitialYPosition = Math.random() * getEnemyMaximumYPosition();
-                DestructibleEntity newEnemy = new EnemyPlane(getScreenWidth(), newEnemyInitialYPosition);
+                DestructibleEntity newEnemy;
+                if (enemyCounter % 2 == 0) {
+                    newEnemy = new EnemyPlane(getScreenWidth(), newEnemyInitialYPosition);
+                } else {
+                    newEnemy = new AdvancedEnemyPlane(getScreenWidth(), newEnemyInitialYPosition);
+                }
                 addEnemyUnit(newEnemy);
+                enemyCounter++;
             }
         }
     }
