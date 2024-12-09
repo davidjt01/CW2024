657a68da9df1015122da84ec8b6050248e792de8
davidjt <davidjt001@yahoo.com>
2024-12-09 15:23:03 +0800

    added AdvancedEnemyPlane

---
+++ src/main/java/com/example/demo/planes/AdvancedEnemyPlane.java
@@ -0,0 +1,58 @@
+package com.example.demo.planes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class AdvancedEnemyPlane extends EnemyPlane {
+    private static final int VERTICAL_VELOCITY = 8;
+    private static final int MOVE_FREQUENCY_PER_CYCLE = 5;
+    private static final int MAX_FRAMES_WITH_SAME_MOVE = 10;
+    private static final int Y_POSITION_UPPER_BOUND = 10;
+    private static final int Y_POSITION_LOWER_BOUND = 475;
+    private final List<Integer> movePattern;
+    private int consecutiveMovesInSameDirection;
+    private int indexOfCurrentMove;
+
+    public AdvancedEnemyPlane(double initialXPos, double initialYPos) {
+        super(initialXPos, initialYPos);
+        movePattern = new ArrayList<>();
+        consecutiveMovesInSameDirection = 0;
+        indexOfCurrentMove = 0;
+        initializeMovePattern();
+    }
+
+    @Override
+    public void updatePosition() {
+        super.updatePosition(); // Keep horizontal movement
+        double initialTranslateY = getTranslateY();
+        moveVertically(getNextMove());
+        double currentPosition = getLayoutY() + getTranslateY();
+        if (currentPosition < Y_POSITION_UPPER_BOUND || currentPosition > Y_POSITION_LOWER_BOUND) {
+            setTranslateY(initialTranslateY);
+        }
+    }
+
+    private void initializeMovePattern() {
+        for (int i = 0; i < MOVE_FREQUENCY_PER_CYCLE; i++) {
+            movePattern.add(VERTICAL_VELOCITY);
+            movePattern.add(-VERTICAL_VELOCITY);
+            movePattern.add(0);
+        }
+        Collections.shuffle(movePattern);
+    }
+
+    private int getNextMove() {
+        int currentMove = movePattern.get(indexOfCurrentMove);
+        consecutiveMovesInSameDirection++;
+        if (consecutiveMovesInSameDirection == MAX_FRAMES_WITH_SAME_MOVE) {
+            Collections.shuffle(movePattern);
+            consecutiveMovesInSameDirection = 0;
+            indexOfCurrentMove++;
+        }
+        if (indexOfCurrentMove == movePattern.size()) {
+            indexOfCurrentMove = 0;
+        }
+        return currentMove;
+    }
+}
