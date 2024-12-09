15e18253143462d95ddd1317d417599282b2f87b
davidjt <davidjt001@yahoo.com>
2024-12-09 17:41:55 +0800

    Added LevelMenu MainMenu and Controller interface and renamed Controller to GameController and updated GameController and Main

--- src/main/java/com/example/demo/controller/Main.java
+++ src/main/java/com/example/demo/controller/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 	private static final int SCREEN_WIDTH = 1300;
 	private static final int SCREEN_HEIGHT = 750;
 	private static final String TITLE = "Sky Battle";
-	private Controller myController;
+	private GameController myGameController;
 
 	@Override
 	public void start(Stage stage) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
@@ -19,8 +19,8 @@ public class Main extends Application {
 		stage.setResizable(false);
 		stage.setHeight(SCREEN_HEIGHT);
 		stage.setWidth(SCREEN_WIDTH);
-		myController = new Controller(stage);
-		myController.launchGame();
+		myGameController = new GameController(stage);
+		myGameController.launchGame();
 	}
 
 	public static void main(String[] args) {
