a533df681c5e4296f1e9261da8ee343bd17a7066
davidjt <davidjt001@yahoo.com>
2024-12-11 07:38:56 +0800

    updated LevelFive to spawn EnemyPlane and AdvancedEnemyPlane

--- src/main/java/com/example/demo/levels/LevelFive.java
+++ src/main/java/com/example/demo/levels/LevelFive.java
@@ -1,14 +1,20 @@
 package com.example.demo.levels;
 
 import com.example.demo.controller.Controller;
+import com.example.demo.entities.DestructibleEntity;
 import com.example.demo.levelviews.LevelView;
 import com.example.demo.levelviews.BossLevelView;
+import com.example.demo.planes.AdvancedEnemyPlane;
+import com.example.demo.planes.EnemyPlane;
 import com.example.demo.planes.FinalBossPlane;
 
 public class LevelFive extends LevelParent {
 
     private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background2.jpg";
+    private static final int TOTAL_ENEMIES = 5;
+    private static final double ENEMY_SPAWN_PROBABILITY = .20;
     private static final int PLAYER_INITIAL_HEALTH = 5;
+    private int enemyCounter = 0;
     private final FinalBossPlane finalBossPlane;
     private BossLevelView levelView;
 
@@ -34,6 +40,20 @@ public class LevelFive extends LevelParent {
 
     @Override
     protected void spawnEnemyUnits() {
+        int currentNumberOfEnemies = getCurrentNumberOfEnemies();
+        for (int i = 0; i < TOTAL_ENEMIES - currentNumberOfEnemies; i++) {
+            if (Math.random() < ENEMY_SPAWN_PROBABILITY) {
+                double newEnemyInitialYPosition = Math.random() * getEnemyMaximumYPosition();
+                DestructibleEntity newEnemy;
+                if (enemyCounter % 2 == 0) {
+                    newEnemy = new EnemyPlane(getScreenWidth(), newEnemyInitialYPosition);
+                } else {
+                    newEnemy = new AdvancedEnemyPlane(getScreenWidth(), newEnemyInitialYPosition);
+                }
+                addEnemyUnit(newEnemy);
+                enemyCounter++;
+            }
+        }
         if (getCurrentNumberOfEnemies() == 0) {
             addEnemyUnit(finalBossPlane);
         }
@@ -44,5 +64,4 @@ public class LevelFive extends LevelParent {
         levelView = new BossLevelView(getRoot(), PLAYER_INITIAL_HEALTH);
         return levelView;
     }
-
 }
