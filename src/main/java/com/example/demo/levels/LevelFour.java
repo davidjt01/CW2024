a21ba182075da4f2a22e3257484d77f2f2d2b092
davidjt <davidjt001@yahoo.com>
2024-12-11 07:49:59 +0800

    updated LevelFour to also sapwn EnemyPlane

--- src/main/java/com/example/demo/levels/LevelFour.java
+++ src/main/java/com/example/demo/levels/LevelFour.java
@@ -1,15 +1,19 @@
 package com.example.demo.levels;
 
 import com.example.demo.controller.Controller;
+import com.example.demo.entities.DestructibleEntity;
 import com.example.demo.levelviews.LevelView;
 import com.example.demo.levelviews.BossLevelView;
 import com.example.demo.planes.SecondBossPlane;
+import com.example.demo.planes.EnemyPlane;
 
 public class LevelFour extends LevelParent {
 
     private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background2.jpg";
     private static final String NEXT_LEVEL = "com.example.demo.levels.LevelFive";
     private static final int PLAYER_INITIAL_HEALTH = 5;
+    private static final int TOTAL_ENEMIES = 5;
+    private static final double ENEMY_SPAWN_PROBABILITY = .20;
     private final SecondBossPlane secondBossPlane;
     private BossLevelView levelView;
 
@@ -38,6 +42,15 @@ public class LevelFour extends LevelParent {
         if (getCurrentNumberOfEnemies() == 0) {
             addEnemyUnit(secondBossPlane);
         }
+
+        int currentNumberOfEnemies = getCurrentNumberOfEnemies();
+        for (int i = 0; i < TOTAL_ENEMIES - currentNumberOfEnemies; i++) {
+            if (Math.random() < ENEMY_SPAWN_PROBABILITY) {
+                double newEnemyInitialYPosition = Math.random() * getEnemyMaximumYPosition();
+                DestructibleEntity newEnemy = new EnemyPlane(getScreenWidth(), newEnemyInitialYPosition);
+                addEnemyUnit(newEnemy);
+            }
+        }
     }
 
     @Override
@@ -45,5 +58,4 @@ public class LevelFour extends LevelParent {
         levelView = new BossLevelView(getRoot(), PLAYER_INITIAL_HEALTH);
         return levelView;
     }
-
 }
