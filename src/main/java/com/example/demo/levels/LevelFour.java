e4532e2328c2514f5ea7cf9274c6d096d0b4f52b
davidjt <davidjt001@yahoo.com>
2024-12-11 07:29:32 +0800

    updated LevelFour to spawn two second boss planes instead of one

--- src/main/java/com/example/demo/levels/LevelFour.java
+++ src/main/java/com/example/demo/levels/LevelFour.java
@@ -10,12 +10,14 @@ public class LevelFour extends LevelParent {
     private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background2.jpg";
     private static final String NEXT_LEVEL = "com.example.demo.levels.LevelFive";
     private static final int PLAYER_INITIAL_HEALTH = 5;
-    private final SecondBossPlane secondBossPlane;
+    private final SecondBossPlane secondBossPlane1;
+    private final SecondBossPlane secondBossPlane2;
     private BossLevelView levelView;
 
     public LevelFour(Controller gameController, double screenHeight, double screenWidth) {
         super(gameController, BACKGROUND_IMAGE_NAME, screenHeight, screenWidth, PLAYER_INITIAL_HEALTH);
-        secondBossPlane = new SecondBossPlane(levelView);
+        secondBossPlane1 = new SecondBossPlane(levelView);
+        secondBossPlane2 = new SecondBossPlane(levelView);
     }
 
     @Override
@@ -28,7 +30,7 @@ public class LevelFour extends LevelParent {
         if (userIsDestroyed()) {
             loseGame();
         }
-        else if (secondBossPlane.isDestroyed()) {
+        else if (secondBossPlane1.isDestroyed() && secondBossPlane2.isDestroyed()) {
             goToNextLevel(NEXT_LEVEL);
         }
     }
@@ -36,7 +38,8 @@ public class LevelFour extends LevelParent {
     @Override
     protected void spawnEnemyUnits() {
         if (getCurrentNumberOfEnemies() == 0) {
-            addEnemyUnit(secondBossPlane);
+            addEnemyUnit(secondBossPlane1);
+            addEnemyUnit(secondBossPlane2);
         }
     }
 
@@ -45,5 +48,4 @@ public class LevelFour extends LevelParent {
         levelView = new BossLevelView(getRoot(), PLAYER_INITIAL_HEALTH);
         return levelView;
     }
-
 }
