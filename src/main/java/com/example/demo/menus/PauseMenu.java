7f607d4a1d97fbaea978d46811a53eeeb99bc526
davidjt <davidjt001@yahoo.com>
2024-12-10 18:15:40 +0800

    renamed button names for clarity in PauseMenu

--- src/main/java/com/example/demo/menus/PauseMenu.java
+++ src/main/java/com/example/demo/menus/PauseMenu.java
@@ -3,13 +3,10 @@ package com.example.demo.menus;
 import com.example.demo.controller.Controller;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.util.Objects;
 
 public class PauseMenu {
     private final Stage stage;
@@ -23,37 +20,33 @@ public class PauseMenu {
 
     public void show() {
         VBox menuLayout = new VBox(20);
-        menuLayout.setStyle("-fx-alignment: center; -fx-padding: 50; -fx-background-color: black;");
-        //menuLayout.setStyle("-fx-alignment: center; -fx-padding: 50;");
+        menuLayout.setStyle("-fx-alignment: center; -fx-padding: 50;");
 
         Text gameTitle = new Text("Pause");
         gameTitle.setFont(new Font("Arial", 48));
         gameTitle.setStyle("-fx-fill: white;");
 
-        Button playButton = new Button("Continue");
-        playButton.setPrefSize(200, 50);
-        playButton.setStyle("-fx-font-size: 18px;");
-
-        playButton.setOnAction(e -> {
+        Button continueButton = new Button("Continue");
+        continueButton.setPrefSize(200, 50);
+        continueButton.setStyle("-fx-font-size: 18px;");
+        continueButton.setOnAction(e -> {
             stage.setScene(gameScene);
             gameController.onContinueSelected();
         });
 
-        Button backButton = new Button("Back");
-        backButton.setPrefSize(200, 50);
-        backButton.setStyle("-fx-font-size: 18px;");
+        Button levelMenuButton = new Button("Level Menu");
+        levelMenuButton.setPrefSize(200, 50);
+        levelMenuButton.setStyle("-fx-font-size: 18px;");
+        levelMenuButton.setOnAction(e -> gameController.onLevelMenuSelected());
 
-        backButton.setOnAction(e -> gameController.onLevelMenuSelected());
+        menuLayout.getChildren().addAll(gameTitle, continueButton, levelMenuButton);
 
-        menuLayout.getChildren().addAll(gameTitle, playButton, backButton);
-
-        Scene scene =new Scene(menuLayout, stage.getWidth(), stage.getHeight());
+        Scene scene = new Scene(menuLayout, stage.getWidth(), stage.getHeight());
         stage.setScene(scene);
         stage.show();
-    };
+    }
 
     public void saveGameScene(Scene gameScene) {
         this.gameScene = gameScene;
-
     }
 }
