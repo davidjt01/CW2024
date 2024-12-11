4a0c3546107a6004024a1d417c2656fd66eede99
davidjt <davidjt001@yahoo.com>
2024-12-11 14:48:20 +0800

    resolved potential nullpointedexception

--- src/main/java/com/example/demo/entities/Entity.java
+++ src/main/java/com/example/demo/entities/Entity.java
@@ -3,13 +3,15 @@ package com.example.demo.entities;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.util.Objects;
+
 public abstract class Entity extends ImageView {
 
     private static final String IMAGE_LOCATION = "/com/example/demo/images/";
 
     public Entity(String imageName, int imageHeight, double initialXPos, double initialYPos) {
         //this.setImage(new Image(IMAGE_LOCATION + imageName));
-        this.setImage(new Image(getClass().getResource(IMAGE_LOCATION + imageName).toExternalForm()));
+        this.setImage(new Image(Objects.requireNonNull(getClass().getResource(IMAGE_LOCATION + imageName)).toExternalForm()));
         this.setLayoutX(initialXPos);
         this.setLayoutY(initialYPos);
         this.setFitHeight(imageHeight);
