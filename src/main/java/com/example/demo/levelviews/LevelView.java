bbb26bf597c5a97ee6b31e914ee0869f17217a21
davidjt <davidjt001@yahoo.com>
2024-12-11 14:59:01 +0800

    removed attributes and methods relating to windisplay and gameoverdisplay in LevelView as they are handled in WinMenu and GameOverMenu

--- src/main/java/com/example/demo/levelviews/LevelView.java
+++ src/main/java/com/example/demo/levelviews/LevelView.java
@@ -7,20 +7,12 @@ public class LevelView {
 
     private static final double HEART_DISPLAY_X_POSITION = 5;
     private static final double HEART_DISPLAY_Y_POSITION = 25;
-    private static final int WIN_IMAGE_X_POSITION = 355;
-    private static final int WIN_IMAGE_Y_POSITION = 175;
-    private static final int LOSS_SCREEN_X_POSITION = -160;
-    private static final int LOSS_SCREEN_Y_POSITION = -375;
     private final Group root;
-    //private final WinDisplay winDisplay;
-    //private final GameOverDisplay gameOverDisplay;
     private final HeartDisplay heartDisplay;
 
     LevelView(Group root, int heartsToDisplay) {
         this.root = root;
         this.heartDisplay = HeartDisplay.createHeartDisplay(HEART_DISPLAY_X_POSITION, HEART_DISPLAY_Y_POSITION, heartsToDisplay);
-        //this.winDisplay = new WinDisplay(WIN_IMAGE_X_POSITION, WIN_IMAGE_Y_POSITION);
-        //this.gameOverDisplay = new GameOverDisplay(LOSS_SCREEN_X_POSITION, LOSS_SCREEN_Y_POSITION);
     }
 
     public static LevelView createLevelView(Group root, int heartsToDisplay) {
@@ -29,15 +21,6 @@ public class LevelView {
 
     public void showHeartDisplay() {
         root.getChildren().add(heartDisplay.getContainer());
-    }
-
-    public void showWinImage() {
-        //root.getChildren().add(winDisplay);
-        //winDisplay.showWinImage();
-    }
-
-    public void showGameOverImage() {
-        //root.getChildren().add(gameOverDisplay);
     }
 
     public void removeHearts(int heartsRemaining) {
