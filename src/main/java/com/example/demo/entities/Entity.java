1d743b5be9ff682f13e8c4979b65ad45ade0d5fb
davidjt <davidjt001@yahoo.com>
2024-12-11 20:19:55 +0800

    added javadocs for Entity

--- src/main/java/com/example/demo/entities/Entity.java
+++ src/main/java/com/example/demo/entities/Entity.java
@@ -5,10 +5,19 @@ import javafx.scene.image.ImageView;
 
 import java.util.Objects;
 
+/**
+ *
+ */
 public abstract class Entity extends ImageView {
 
     private static final String IMAGE_LOCATION = "/com/example/demo/images/";
 
+    /**
+     * @param imageName
+     * @param imageHeight
+     * @param initialXPos
+     * @param initialYPos
+     */
     public Entity(String imageName, int imageHeight, double initialXPos, double initialYPos) {
         //this.setImage(new Image(IMAGE_LOCATION + imageName));
         this.setImage(new Image(Objects.requireNonNull(getClass().getResource(IMAGE_LOCATION + imageName)).toExternalForm()));
@@ -18,12 +27,21 @@ public abstract class Entity extends ImageView {
         this.setPreserveRatio(true);
     }
 
+    /**
+     *
+     */
     public abstract void updatePosition();
 
+    /**
+     * @param horizontalMove
+     */
     protected void moveHorizontally(double horizontalMove) {
         this.setTranslateX(getTranslateX() + horizontalMove);
     }
 
+    /**
+     * @param verticalMove
+     */
     protected void moveVertically(double verticalMove) {
         this.setTranslateY(getTranslateY() + verticalMove);
     }
