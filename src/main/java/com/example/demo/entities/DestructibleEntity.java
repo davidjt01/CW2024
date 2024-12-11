5e6a243c77c1037ecbc8562fa156c4fedef9bf73
davidjt <davidjt001@yahoo.com>
2024-12-11 20:23:33 +0800

    updated javadocs for DestructibleEntity

--- src/main/java/com/example/demo/entities/DestructibleEntity.java
+++ src/main/java/com/example/demo/entities/DestructibleEntity.java
@@ -1,17 +1,19 @@
 package com.example.demo.entities;
 
 /**
- *
+ * The {@code DestructibleEntity} class represents an entity that can take damage and be destroyed.
+ * It extends {@link Entity} and implements the {@link Destructible} interface.
  */
 public abstract class DestructibleEntity extends Entity implements Destructible {
-
     private boolean isDestroyed;
 
     /**
-     * @param imageName
-     * @param imageHeight
-     * @param initialXPos
-     * @param initialYPos
+     * Constructs a {@code DestructibleEntity} with the specified image, height, and initial position.
+     *
+     * @param imageName the name of the image file.
+     * @param imageHeight the height of the image.
+     * @param initialXPos the initial x-coordinate of the entity.
+     * @param initialYPos the initial y-coordinate of the entity.
      */
     public DestructibleEntity(String imageName, int imageHeight, double initialXPos, double initialYPos) {
         super(imageName, imageHeight, initialXPos, initialYPos);
@@ -19,24 +21,28 @@ public abstract class DestructibleEntity extends Entity implements Destructible 
     }
 
     /**
-     *
+     * Updates the position of the entity.
+     * This method should be implemented by subclasses to define how the entity moves.
      */
     @Override
     public abstract void updatePosition();
 
     /**
-     *
+     * Updates the state of the entity.
+     * This method should be implemented by subclasses to define how the entity behaves.
      */
     public abstract void updateActor();
 
     /**
-     *
+     * Inflicts damage on the entity.
+     * This method should be implemented by subclasses to define how the entity takes damage.
      */
     @Override
     public abstract void takeDamage();
 
     /**
-     *
+     * Destroys the entity.
+     * This method sets the entity's destroyed state to true.
      */
     @Override
     public void destroy() {
@@ -44,17 +50,18 @@ public abstract class DestructibleEntity extends Entity implements Destructible 
     }
 
     /**
-     * @return
+     * Checks if the entity is destroyed.
+     *
+     * @return {@code true} if the entity is destroyed, {@code false} otherwise.
      */
     public boolean isDestroyed() {
         return isDestroyed;
     }
 
     /**
-     *
+     * Sets the entity's destroyed state to true.
      */
     protected void setDestroyed() {
         this.isDestroyed = true;
     }
-
 }
