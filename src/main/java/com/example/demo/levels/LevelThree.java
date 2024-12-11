f257f23bc97268aad96730f7df474230faab38a2
davidjt <davidjt001@yahoo.com>
2024-12-11 12:34:32 +0800

    increase kills to advance in LevelThree

--- src/main/java/com/example/demo/levels/LevelThree.java
+++ src/main/java/com/example/demo/levels/LevelThree.java
@@ -11,7 +11,7 @@ public class LevelThree extends LevelParent {
     private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background1.jpg";
     private static final String NEXT_LEVEL = "com.example.demo.levels.LevelFour";
     private static final int TOTAL_ENEMIES = 5;
-    private static final int KILLS_TO_ADVANCE = 10;
+    private static final int KILLS_TO_ADVANCE = 20;
     private static final double ENEMY_SPAWN_PROBABILITY = .20;
     private static final int PLAYER_INITIAL_HEALTH = 5;
     private int enemyCounter = 0; // Counter to alternate between enemy types
