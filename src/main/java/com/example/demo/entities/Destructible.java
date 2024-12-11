4a34fd3c25c0ed5cb344d948cb22504c2fa28798
davidjt <davidjt001@yahoo.com>
2024-12-11 20:17:33 +0800

    update javadocs for Destructible

--- src/main/java/com/example/demo/entities/Destructible.java
+++ src/main/java/com/example/demo/entities/Destructible.java
@@ -1,17 +1,20 @@
 package com.example.demo.entities;
 
 /**
- *
+ * The {@code Destructible} interface defines the methods required for an entity
+ * that can take damage and be destroyed in the game.
  */
 public interface Destructible {
 
     /**
-     *
+     * Inflicts damage on the entity.
+     * This method should be called when the entity takes damage.
      */
     void takeDamage();
 
     /**
-     *
+     * Destroys the entity.
+     * This method should be called when the entity is destroyed.
      */
     void destroy();
 
