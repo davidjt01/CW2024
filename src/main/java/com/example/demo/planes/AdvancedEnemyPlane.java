4108f83b97ab3d223fb44befb2a5738b8d607308
davidjt <davidjt001@yahoo.com>
2024-12-11 12:45:22 +0800

    adjusted VERTICAL_VELOCITY in AdvancedEnemyPlane

--- src/main/java/com/example/demo/planes/AdvancedEnemyPlane.java
+++ src/main/java/com/example/demo/planes/AdvancedEnemyPlane.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class AdvancedEnemyPlane extends EnemyPlane {
     private static final String IMAGE_NAME = "advancedenemyplane.png";
     private static final int HORIZONTAL_VELOCITY = -10;
-    private static final int VERTICAL_VELOCITY = 8;
+    private static final int VERTICAL_VELOCITY = 6;
     private static final int MOVE_FREQUENCY_PER_CYCLE = 5;
     private static final int MAX_FRAMES_WITH_SAME_MOVE = 10;
     private static final int Y_POSITION_UPPER_BOUND = 10;
