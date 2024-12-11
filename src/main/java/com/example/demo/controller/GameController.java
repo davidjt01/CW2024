cce0db307638e87968a4b5fd2ef0130f65650786
davidjt <davidjt001@yahoo.com>
2024-12-11 18:16:06 +0800

    added javadocs for GameController

--- src/main/java/com/example/demo/controller/GameController.java
+++ src/main/java/com/example/demo/controller/GameController.java
@@ -14,39 +14,76 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.Observable;
 import java.util.Observer;
 
+/**
+ *
+ */
 public class GameController implements Observer, Controller {
 
     private final Stage stage;
     private LevelParent currLevel;
 
+    /**
+     *
+     * @param stage
+     */
     private GameController(Stage stage) {
         this.stage = stage;
     }
 
+    /**
+     *
+     * @param stage
+     * @return
+     */
     public static GameController createGameController(Stage stage) {
         return new GameController(stage);
     }
 
+    /**
+     *
+     * @throws SecurityException
+     * @throws IllegalArgumentException
+     */
     public void launchGame() throws SecurityException,
             IllegalArgumentException {
         showMainMenu();
     }
 
+    /**
+     *
+     */
     private void showMainMenu() {
         MainMenu mainMenu = new MainMenu(stage, this);
         mainMenu.show();
     }
 
+    /**
+     *
+     */
     private void showLevelMenu() {
         LevelMenu levelMenu = new LevelMenu(stage, this);
         levelMenu.show();
     }
 
+    /**
+     *
+     */
     private void showSettingsMenu() {
         SettingsMenu settingsMenu = new SettingsMenu(stage, this);
         settingsMenu.show();
     }
 
+    /**
+     *
+     * @param className
+     * @throws ClassNotFoundException
+     * @throws NoSuchMethodException
+     * @throws SecurityException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     * @throws InvocationTargetException
+     */
     private void goToLevel(String className) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
             InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 
@@ -60,6 +97,12 @@ public class GameController implements Observer, Controller {
 
     }
 
+    /**
+     *
+     * @param arg0     the observable object.
+     * @param arg1   an argument passed to the {@code notifyObservers}
+     *                 method.
+     */
     @Override
     public void update(Observable arg0, Object arg1) {
         try {
@@ -72,6 +115,10 @@ public class GameController implements Observer, Controller {
         }
     }
 
+    /**
+     *
+     * @param levelName the name of the selected level.
+     */
     public void onLevelSelected(String levelName) {
         try {
             System.out.println("go to level " + levelName);
@@ -83,19 +130,31 @@ public class GameController implements Observer, Controller {
         }
     }
 
+    /**
+     *
+     */
     public void onMainMenuSelected() {
         System.out.println("main menu selected");
         showMainMenu();
     }
 
+    /**
+     *
+     */
     public void onLevelMenuSelected() {
         showLevelMenu();
     }
 
+    /**
+     *
+     */
     public void onContinueSelected() {
         currLevel.resumeGame();
     }
 
+    /**
+     *
+     */
     public void onSettingsMenuSelected() {
         showSettingsMenu();
     }
