53e46c1d80a7ada7d1d2ea8a0c1581071a487407
davidjt <davidjt001@yahoo.com>
2024-12-11 08:10:28 +0800

    udpated Secondbossplane to override method updateUElements to adjust shield and healthbar offset values

--- src/main/java/com/example/demo/planes/SecondBossPlane.java
+++ src/main/java/com/example/demo/planes/SecondBossPlane.java
@@ -8,6 +8,10 @@ public class SecondBossPlane extends BossPlane {
 
     private static final String IMAGE_NAME = "secondbossplane.png";
     private static final int IMAGE_HEIGHT = 70;
+    private static final double SHIELD_OFFSET_X = 40.0;
+    private static final double SHIELD_OFFSET_Y = -40.0;
+    private static final double HEALTH_BAR_OFFSET_X = 35.0;
+    private static final double HEALTH_BAR_OFFSET_Y = -40.0;
 
     public SecondBossPlane(BossLevelView levelView) {
         super(IMAGE_NAME, IMAGE_HEIGHT, levelView);
@@ -16,5 +20,13 @@ public class SecondBossPlane extends BossPlane {
     @Override
     public DestructibleEntity fireProjectile() {
         return bossFiresInCurrentFrame() ? new RandomMovingBossProjectile(getProjectileInitialPosition()) : null;
+    }
+
+    @Override
+    protected void updateUIElements() {
+        double x = getLayoutX() + getTranslateX();
+        double y = getLayoutY() + getTranslateY();
+        levelView.setShieldPosition(x + SHIELD_OFFSET_X, y + SHIELD_OFFSET_Y);
+        levelView.setHealthBarPosition(x + HEALTH_BAR_OFFSET_X, y + HEALTH_BAR_OFFSET_Y);
     }
 }
