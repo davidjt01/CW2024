3c38dca350419b9d5c5759566edbe9402e8a7af6
davidjt <davidjt001@yahoo.com>
2024-12-11 17:56:44 +0800

    added javadocs in Controller

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,13 +1,31 @@
 package com.example.demo.controller;
 
+/**
+ *
+ */
 public interface Controller {
+    /**
+     * @param level
+     */
     void onLevelSelected(String level);
 
+    /**
+     *
+     */
     void onMainMenuSelected();
 
+    /**
+     *
+     */
     void onLevelMenuSelected();
 
+    /**
+     *
+     */
     void onContinueSelected();
 
+    /**
+     *
+     */
     void onSettingsMenuSelected();
 }
