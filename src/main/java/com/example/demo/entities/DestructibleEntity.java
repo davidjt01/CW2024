3c4cc5bcc5770603d3e18460bae58ea6fccd26a1
davidjt <davidjt001@yahoo.com>
2024-12-11 20:22:43 +0800

    added javadocs for DestructibleEntity

--- src/main/java/com/example/demo/entities/DestructibleEntity.java
+++ src/main/java/com/example/demo/entities/DestructibleEntity.java
@@ -1,31 +1,58 @@
 package com.example.demo.entities;
 
+/**
+ *
+ */
 public abstract class DestructibleEntity extends Entity implements Destructible {
 
     private boolean isDestroyed;
 
+    /**
+     * @param imageName
+     * @param imageHeight
+     * @param initialXPos
+     * @param initialYPos
+     */
     public DestructibleEntity(String imageName, int imageHeight, double initialXPos, double initialYPos) {
         super(imageName, imageHeight, initialXPos, initialYPos);
         isDestroyed = false;
     }
 
+    /**
+     *
+     */
     @Override
     public abstract void updatePosition();
 
+    /**
+     *
+     */
     public abstract void updateActor();
 
+    /**
+     *
+     */
     @Override
     public abstract void takeDamage();
 
+    /**
+     *
+     */
     @Override
     public void destroy() {
         setDestroyed();
     }
 
+    /**
+     * @return
+     */
     public boolean isDestroyed() {
         return isDestroyed;
     }
 
+    /**
+     *
+     */
     protected void setDestroyed() {
         this.isDestroyed = true;
     }
