460db9f5c996adfc0cc96929daf0641fb06c4d94
davidjt <davidjt001@yahoo.com>
2024-12-11 20:20:22 +0800

    updated javadocs for Entity

--- src/main/java/com/example/demo/entities/Entity.java
+++ src/main/java/com/example/demo/entities/Entity.java
@@ -6,20 +6,22 @@ import javafx.scene.image.ImageView;
 import java.util.Objects;
 
 /**
- *
+ * The {@code Entity} class represents a game entity with an image.
+ * It extends {@link ImageView} and provides methods to update the entity's position.
  */
 public abstract class Entity extends ImageView {
 
     private static final String IMAGE_LOCATION = "/com/example/demo/images/";
 
     /**
-     * @param imageName
-     * @param imageHeight
-     * @param initialXPos
-     * @param initialYPos
+     * Constructs an {@code Entity} with the specified image, height, and initial position.
+     *
+     * @param imageName the name of the image file.
+     * @param imageHeight the height of the image.
+     * @param initialXPos the initial x-coordinate of the entity.
+     * @param initialYPos the initial y-coordinate of the entity.
      */
     public Entity(String imageName, int imageHeight, double initialXPos, double initialYPos) {
-        //this.setImage(new Image(IMAGE_LOCATION + imageName));
         this.setImage(new Image(Objects.requireNonNull(getClass().getResource(IMAGE_LOCATION + imageName)).toExternalForm()));
         this.setLayoutX(initialXPos);
         this.setLayoutY(initialYPos);
@@ -28,19 +30,24 @@ public abstract class Entity extends ImageView {
     }
 
     /**
-     *
+     * Updates the position of the entity.
+     * This method should be implemented by subclasses to define how the entity moves.
      */
     public abstract void updatePosition();
 
     /**
-     * @param horizontalMove
+     * Moves the entity horizontally by the specified amount.
+     *
+     * @param horizontalMove the amount to move the entity horizontally.
      */
     protected void moveHorizontally(double horizontalMove) {
         this.setTranslateX(getTranslateX() + horizontalMove);
     }
 
     /**
-     * @param verticalMove
+     * Moves the entity vertically by the specified amount.
+     *
+     * @param verticalMove the amount to move the entity vertically.
      */
     protected void moveVertically(double verticalMove) {
         this.setTranslateY(getTranslateY() + verticalMove);
