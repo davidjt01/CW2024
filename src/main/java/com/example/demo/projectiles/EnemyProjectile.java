0855ad55efa7db468f07067a57ae0c7c090a9d69
davidjt <davidjt001@yahoo.com>
2024-12-12 10:14:06 +0800

    add javadocs for EnemyProjectile

--- src/main/java/com/example/demo/projectiles/EnemyProjectile.java
+++ src/main/java/com/example/demo/projectiles/EnemyProjectile.java
@@ -1,24 +1,39 @@
 package com.example.demo.projectiles;
 
+/**
+ * The {@code EnemyProjectile} class represents a projectile fired by enemy planes in the game.
+ * It extends {@link Projectile} and provides methods for updating its position and handling its behavior.
+ */
 public class EnemyProjectile extends Projectile {
-
     private static final String IMAGE_NAME = "enemyFire.png";
     private static final int IMAGE_HEIGHT = 32;
     private static final int HORIZONTAL_VELOCITY = -10;
 
+    /**
+     * Constructs an {@code EnemyProjectile} with the specified initial x and y positions.
+     *
+     * @param initialXPos the initial x-coordinate of the enemy projectile.
+     * @param initialYPos the initial y-coordinate of the enemy projectile.
+     */
     public EnemyProjectile(double initialXPos, double initialYPos) {
         super(IMAGE_NAME, IMAGE_HEIGHT, initialXPos, initialYPos);
     }
 
+    /**
+     * Updates the position of the enemy projectile.
+     * Moves the projectile horizontally based on its velocity.
+     */
     @Override
     public void updatePosition() {
         moveHorizontally(HORIZONTAL_VELOCITY);
     }
 
+    /**
+     * Updates the state of the enemy projectile.
+     * This method is called in each frame to update the projectile's behavior.
+     */
     @Override
     public void updateActor() {
         updatePosition();
     }
-
-
 }
