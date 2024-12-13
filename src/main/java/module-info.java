115054ac1d043cf7528e47e87229fc5e45ca6eed
davidjt <davidjt001@yahoo.com>
2024-12-13 13:51:30 +0800

    module info test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,21 +3,14 @@ module com.example.demo {
     requires javafx.fxml;
     requires javafx.media;
 
-    // Required for testing
-    requires org.junit.jupiter.api;
-    requires org.mockito;
-    requires org.testfx.junit5;
-    requires org.junit.platform.commons;
 
-    // Exports
+    //opens com.example.demo to javafx.fxml;
     exports com.example.demo.controller;
-
-    // Opens for reflection during tests
-    opens com.example.demo.planes to javafx.fxml, org.junit.jupiter.api;
-    opens com.example.demo.levels to javafx.fxml, org.junit.jupiter.api;
-    opens com.example.demo.displays to javafx.fxml, org.junit.jupiter.api;
-    opens com.example.demo.projectiles to javafx.fxml, org.junit.jupiter.api;
-    opens com.example.demo.entities to javafx.fxml, org.junit.jupiter.api;
-    opens com.example.demo.levelviews to javafx.fxml, org.junit.jupiter.api;
-    opens com.example.demo.audio to javafx.fxml, org.junit.jupiter.api;
+    opens com.example.demo.planes to javafx.fxml;
+    opens com.example.demo.levels to javafx.fxml;
+    opens com.example.demo.displays to javafx.fxml;
+    opens com.example.demo.projectiles to javafx.fxml;
+    opens com.example.demo.entities to javafx.fxml;
+    opens com.example.demo.levelviews to javafx.fxml;
+    opens com.example.demo.audio to javafx.fxml;
 }
